93db92aa96bbda001af7b59ac3cfb65b258ed9e2
alice02 <scene.4913@gmail.com>
2018-04-28 18:37:10 +0900

    Add request parse method (WIP)

--- source/http_request.d
+++ source/http_request.d
@@ -1,8 +1,13 @@
 import std.socket;
 
+import std.stdio;
+import std.string;
+import std.array : array;
+
 class HTTPRequest {
   private:
   Socket client_;
+  string data_;
 
   public:
   this(Socket client) {
@@ -10,8 +15,26 @@ class HTTPRequest {
   }
 
   char[1024] read() {
+    // TODO: Fix buffer length to read
     char[1024] buffer;
     client_.receive(buffer);
+    data_ = cast(string)buffer;
     return buffer;
   }
+
+  string getPath() {
+    auto statusLine = lineSplitter(data_).array[0];
+    writeln(statusLine);
+    auto path = statusLine.split(" ")[1];
+    return path;
+  }
+
+  void getHeader() {
+
+  }
+
+  void getBody() {
+
+  }
+
 }
